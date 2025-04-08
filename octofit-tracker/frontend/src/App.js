3ff77fc078f1d9a35e2fb1250d7eedab8e687d9b
NarutoRoll <jbless85@gmail.com>
2025-04-08 17:01:56 +0000

    URL Autofix

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <Link className="navbar-brand d-flex align-items-center" to="/">
-              <img src="/octofitapp-small.png" alt="OctoFit Logo" className="logo me-2" />
+              <img src={`${process.env.PUBLIC_URL}/octofitapp-small.png`} alt="OctoFit Logo" className="logo me-2" />
               OctoFit Tracker
             </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
